780baeb5c30e96bffc4ce29e28356ae00b40827e
Stewart Barron <stewartbarron@Stewarts-iMac.local>
2021-12-14 18:01:17 +0000

    resilienc4j

---
+++ src/main/java/com/github/stewartbarron/aruna/currencyexchangeservice/controller/CircuitBreakerController.java
@@ -0,0 +1,29 @@
+package com.github.stewartbarron.aruna.currencyexchangeservice.controller;
+
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class CircuitBreakerController {
+
+    private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
+
+    @GetMapping("/sample-api")
+//    @Retry(name = "sample-api", fallbackMethod = "hardCodedResponse")
+    @CircuitBreaker(name = "default ", fallbackMethod = "hardCodedResponse")
+    public String sampleApi() {
+        logger.info("Sample API call received");
+        ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-resource", String.class);
+        return forEntity.getBody();
+    }
+
+    public String hardCodedResponse(Exception ex) {
+        return "fallback response";
+    }
+}
